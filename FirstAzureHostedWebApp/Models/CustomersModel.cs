0380fc162bc6da835670b8b71e30364f13da65c7
Ujjwal-2910 <ujjwalagrawal29102001@gmail.com>
2024-05-06 09:44:55 +0530

    Hosting in Azure through Git

---
+++ FirstAzureHostedWebApp/Models/CustomersModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstAzureHostedWebApp.Models
+{
+    public class Customer
+    {
+        public string? CustomerId { get; set; }
+        public string? CompanyName { get; set; }
+        public string? ContactName { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+    }
+
+    public class CustomersDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public CustomersDbContext(DbContextOptions<CustomersDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
